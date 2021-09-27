b3e0de5ce3c234c7f065fea66ed4336efe869e37
Aleksandr Alekseev <mynickjasper@yandex.ru>
2021-09-27 13:25:02 +0300

    Binding VK Core 3.3.0

---
+++ VkSdk/VKCallback.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.Runtime;
+using Com.VK.Api.Sdk.Exceptions;
+using Com.VK.Api.Sdk.Auth;
+
+namespace VKontakte
+{
+
+    public class ActionCallback<T> : Java.Lang.Object, IVKAuthCallback, IJavaObject
+    {
+        private readonly Action<VKAccessToken> onLogin;
+        private readonly Action<VKAuthException> onLoginFailed;
+
+        public ActionCallback(Action<VKAccessToken> onSuccess, Action<VKAuthException> onError)
+        {
+            this.onLogin = onSuccess;
+            this.onLoginFailed = onError;
+        }
+
+        public void OnLogin(VKAccessToken token)
+        {
+            onLogin?.Invoke(token);
+        }
+
+        public void OnLoginFailed(VKAuthException authException)
+        {
+            onLoginFailed?.Invoke(authException);
+        }
+    }
+}
---
+++ VkSdk/VKSdk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Com.VK.Api.Sdk.Exceptions;
+using Com.VK.Api.Sdk.Auth;
+using Com.VK.Api.Sdk;
+
+namespace VKontakte
+{
+	public partial class VkSdk
+    {
+        public static bool OnActivityResult(int requestCode, Result resultCode, Intent data, ActionCallback<VKAccessToken> vkCallback)
+        {
+            return VK.OnActivityResult(requestCode, (int)resultCode, data, vkCallback);
+        }
+
+        public static bool OnActivityResult(int requestCode, Result resultCode, Intent data, Action<VKAccessToken> onResult, Action<VKAuthException> onError = null)
+        {
+            return VK.OnActivityResult(requestCode, (int)resultCode, data, new ActionCallback<VKAccessToken>(onResult, onError));
+        }
+
+        public static bool OnActivityResult(int requestCode, Result resultCode, Intent data, Action<VKAccessToken> onResult, Action<object> onError = null)
+        {
+            return VK.OnActivityResult(requestCode, (int)resultCode, data, new ActionCallback<VKAccessToken>(onResult, onError));
+        }
+    }
+}
